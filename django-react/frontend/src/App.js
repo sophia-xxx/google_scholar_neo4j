3749a54968fc1dc8902c6e37779869f0a8f029c5
Nidhi Dholaria <ndholaria@ymail.com>
2020-03-31 14:00:29 -0500

    frontend displays all authors via api

---
+++ django-react/frontend/src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+// const list = [
+//   {
+//     'id': 1,
+//     'title': '1st Item',
+//     'description': 'Description here.'
+//   },
+//   {
+//     'id': 2,
+//     'title': '2nd Item',
+//     'description': 'Another description here.'
+//   },
+//   {
+//     'id': 3,
+//     'title': '3rd Item',
+//     'description': 'Third description here.'
+//   }
+// ];
+
+class App extends Component {
+  state = {
+    data: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/author/all');
+      const data = await res.json();
+      console.log("api success!");
+      this.setState({
+        data
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    console.log("rendering!");
+    return (
+      <div>
+        {this.state.data.map(item => (
+          <div key={item.id}>
+            <h1>{item.name}</h1>
+            <span>{item.affiliation}</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
